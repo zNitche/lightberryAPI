c8d384238e0fe8cb4a08c7edf9a310f7ef0ddfeb
zNitche <53448802+zNitche@users.noreply.github.com>
2024-04-16 22:24:29 +0200

    added background tasks handler

--- lightberry/__init__.py
+++ lightberry/__init__.py
@@ -3,3 +3,4 @@ from lightberry.core.app import App
 from lightberry.core.app_context import AppContext
 from lightberry.core.routing.router import Router
 from lightberry.core.communication.response import Response
+from lightberry.tasks.task_base import TaskBase
