6463bdc03df8ff52f105ceb078a14c87267ffc78
zNitche <53448802+zNitche@users.noreply.github.com>
2024-04-20 21:13:23 +0200

    tweaked setting app context

--- main.py
+++ main.py
@@ -9,26 +9,24 @@ def create_routers(app):
     app.add_router(core)
 
 
-def create_app():
-    app = App()
+def setup_app(app):
+    import tasks
 
-    with AppContext(app):
-        import tasks
-
-        app.add_background_task(tasks.ExampleTask())
-        create_routers(app)
-
-        return app
+    app.add_background_task(tasks.ExampleTask())
+    create_routers(app)
 
 
 def main():
     common_utils.print_debug(f"Free space: {files_utils.get_free_space()} kB",
                              debug_enabled=True)
 
-    app = create_app()
-    server = Server(app=app)
+    app = App()
 
-    server.start()
+    with AppContext(app):
+        setup_app(app)
+
+        server = Server(app=app)
+        server.start()
 
 
 if __name__ == '__main__':
