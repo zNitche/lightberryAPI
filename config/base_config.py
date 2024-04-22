6017a8131ff059a4bd7ccfb1850719263f865469
zNitche <53448802+zNitche@users.noreply.github.com>
2024-04-22 23:02:49 +0200

    v1.1.4

--- config/base_config.py
+++ config/base_config.py
@@ -5,11 +5,13 @@ class BaseConfig:
     @classmethod
     def setup(cls, section, extend=False):
         config_content = config_utils.get_config_section("/lightberry_config.json", section)
-        base_keys = cls.__dict__.keys()
 
-        for key in config_content:
-            if key in base_keys or extend:
-                setattr(cls, key, config_content[key])
+        if config_content:
+            base_keys = cls.__dict__.keys()
+
+            for key in config_content:
+                if key in base_keys or extend:
+                    setattr(cls, key, config_content[key])
 
     @classmethod
     def get(cls, key):
